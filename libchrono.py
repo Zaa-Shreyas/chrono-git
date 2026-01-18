e29d0fcd087fa5d25ed4c22a6c47e5303a60fd1a
Zaa-Shreyas <shrey262004@gmail.com>
2026-01-18 19:43:28 +0530

    Implemented hash-object and cat-file

--- libchrono.py
+++ libchrono.py
@@ -216,3 +216,68 @@ def object_write(obj, repo=None):
             with open (path, "wb") as f:
                 f.write(zlib.compress(result))
     return sha
+
+class GitBlob (GitObject):
+    fmt = b"blob"
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data
+
+argsp = argsubparsers.add_parser("cat-file", help="Provide content of repository objects")
+
+argsp.add_argument("type", metavar="type", choices=["blob", "commit", "tag", "tree"], help="Specify the type")
+
+argsp.add_argument("object", metavar="object", help="The object to display")
+
+def cmd_cat_file(args):
+    repo = repo_find()
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+def cat_file(repo, obj, fmt=None):
+    obj  = object_read(repo, object_find(repo, obj, fmt=fmt))
+    sys.stdout.buffer.write(obj.serialize())
+
+def object_find(repo, name, fmt=None, follow=True):
+    return name
+
+argsp = argsubparsers.add_parser("hash-object", help="Compute object ID and optionally creates a blob from a file")
+
+argsp.add_argument("-t",
+                   metavar="type",
+                   dest="type",
+                   choices=["blob", "commit", "tag", "tree"],
+                   default="blob",
+                   help="Specify the type")
+
+argsp.add_argument("-w",
+                   dest="write",
+                   action="store_true",
+                   help="Actually write the object into the database")
+
+argsp.add_argument("path",
+                   help="Read object from <file>")
+
+def cmd_hash_object(args):
+    if args.write:
+        repo = repo_find()
+    else:
+        repo = None
+
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo)
+        print(sha)
+
+def object_hash(fd, fmt, repo=None):
+    data = fd.read()
+
+    match fmt:
+        case b'commit' : obj=Gitcommit(data)
+        case b'tree'   : obj=GitTree(data)
+        case b'blob'   : obj=GitBlob(data)
+        case b'tag'    : obj=GitTag(data)
+        case _         : raise Exception(f"Unknown type {fmt}!")
+
+    return object_write(obj, repo)
